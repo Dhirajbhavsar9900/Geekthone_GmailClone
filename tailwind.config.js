14cffa2b68b63fb613d737b279fe2d9f3c9aa2b2
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-04-01 15:13:48 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#000000",
+        },
+        secondary: {
+          DEFAULT: "#000000",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
