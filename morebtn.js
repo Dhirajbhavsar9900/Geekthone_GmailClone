2dd248e9637da1e7718e4052fd4abb88b072cf17
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-04-02 15:39:27 +0100

    done

---
+++ morebtn.js
@@ -0,0 +1,13 @@
+// let moreBtn = document.querySelector("#morebtn")
+
+// console.log(moreBtn);
+
+
+// function btnmore () {
+//    let a = document.createElement("div"); 
+
+//    a
+//    console.log(a);
+// }
+
+// btnmore()
---
+++ object aray.js
@@ -0,0 +1,14 @@
+    let obj = [{
+        name: "John",
+        age: 30,
+        city: "New York"
+    }, {
+        name: "Jane",
+        age: 28,
+        city: "London"
+    },];
+
+    // for(let i = 0; i < obj.length; i++){
+    //     console.log(i);
+    // }
+    console.log(obj);
